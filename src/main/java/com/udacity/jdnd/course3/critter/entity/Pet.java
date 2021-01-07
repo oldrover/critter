873a51af274c7e7b647b0e1533995dd477bd0a65
oldrover <oldrover@web.de>
2021-01-07 08:52:25 +0100

    modified pet.java

--- src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
@@ -3,9 +3,7 @@ package com.udacity.jdnd.course3.critter.entity;
 import com.udacity.jdnd.course3.critter.pet.PetType;
 import org.hibernate.annotations.Nationalized;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -15,16 +13,64 @@ public class Pet {
     @GeneratedValue
     private long id;
 
+    @Enumerated(EnumType.STRING)
     private PetType type;
 
     @Nationalized
     private String name;
 
-    private long ownerId;
+    @OneToMany(fetch = FetchType.LAZY)
+    private Customer customer;
 
     private LocalDate birthDate;
 
     private String notes;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
