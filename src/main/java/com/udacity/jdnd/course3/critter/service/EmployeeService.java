bfc2a625ce7a1f849960271c7d619fee83724da7
oldrover <oldrover@web.de>
2021-01-07 14:48:11 +0100

    created EmployeeService

---
+++ src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
@@ -0,0 +1,29 @@
+package com.udacity.jdnd.course3.critter.service;
+
+import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(Long id) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        if(optionalEmployee.isPresent()) {
+            return optionalEmployee.get();
+        } else {
+            throw new EmployeeNotFoundException();
+        }
+
+    }
+}
