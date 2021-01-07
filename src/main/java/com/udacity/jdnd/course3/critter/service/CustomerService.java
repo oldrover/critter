89054fa22145f4dbbfbdcf236ec572c7bf92c678
oldrover <oldrover@web.de>
2021-01-07 11:36:47 +0100

    created CustomerService

---
+++ src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
@@ -0,0 +1,28 @@
+package com.udacity.jdnd.course3.critter.service;
+
+import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+
+}
