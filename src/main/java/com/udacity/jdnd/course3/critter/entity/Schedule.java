c9ea56eb31a4452ec882ca7d9213c6e628f4de6c
oldrover <oldrover@web.de>
2021-01-07 10:55:08 +0100

    added Schedule class

--- src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
@@ -1,4 +1,25 @@
 package com.udacity.jdnd.course3.critter.entity;
 
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
 public class Schedule {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private List<Long> employeeIds;
+
+    private List<Long> petIds;
+
+    private LocalDate date;
+    
+    @Enumerated(EnumType.STRING)
+    private Set<EmployeeSkill> activities;
 }
