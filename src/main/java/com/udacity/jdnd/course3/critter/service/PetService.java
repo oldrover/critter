a72876e6f4d4dfc0e9b19d32c35fc5bbf7a38428
oldrover <oldrover@web.de>
2021-01-07 09:21:50 +0100

    created PetService

---
+++ src/main/java/com/udacity/jdnd/course3/critter/service/PetService.java
@@ -0,0 +1,21 @@
+package com.udacity.jdnd.course3.critter.service;
+
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import com.udacity.jdnd.course3.critter.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class PetService {
+
+    @Autowired
+    private PetRepository petRepository;
+
+
+    public Pet savePet(Pet pet) {
+        return petRepository.save(pet);
+    }
+}
