520bb2bcf93b2a8521e24dce3041906be1ccf3ad
oldrover <oldrover@web.de>
2021-01-07 14:56:49 +0100

    added saveEmployee and getEmployeeById to EmployeeService

--- src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
@@ -5,9 +5,11 @@ import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class EmployeeService {
 
     @Autowired
