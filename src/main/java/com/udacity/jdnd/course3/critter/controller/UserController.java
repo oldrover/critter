787143b3a4f2ec3e30b1f54c2cdfea0af818025f
oldrover <oldrover@web.de>
2021-01-08 13:16:05 +0100

    changed if clause in convertCustomerToCustomerDTO

--- src/main/java/com/udacity/jdnd/course3/critter/controller/UserController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/controller/UserController.java
@@ -3,7 +3,6 @@ package com.udacity.jdnd.course3.critter.controller;
 import com.udacity.jdnd.course3.critter.entity.Customer;
 import com.udacity.jdnd.course3.critter.entity.Employee;
 import com.udacity.jdnd.course3.critter.entity.Pet;
-import com.udacity.jdnd.course3.critter.pet.PetDTO;
 import com.udacity.jdnd.course3.critter.service.CustomerService;
 import com.udacity.jdnd.course3.critter.service.EmployeeService;
 import com.udacity.jdnd.course3.critter.service.PetNotFoundException;
