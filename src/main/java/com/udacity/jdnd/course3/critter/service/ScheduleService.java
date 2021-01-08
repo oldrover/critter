81bf38344cb93073bda9014625dcf3b1d020aedd
oldrover <oldrover@web.de>
2021-01-08 11:32:47 +0100

    created ScheduleService

---
+++ src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
@@ -0,0 +1,27 @@
+package com.udacity.jdnd.course3.critter.service;
+
+import com.udacity.jdnd.course3.critter.entity.Schedule;
+import com.udacity.jdnd.course3.critter.repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ScheduleService {
+
+    @Autowired
+    ScheduleRepository scheduleRepository;
+
+
+    public Schedule saveSchedule(Schedule schedule) {
+        return scheduleRepository.save(schedule);
+
+    }
+
+    public List<Schedule> getAllSchedules() {
+        return scheduleRepository.findAll();
+    }
+}
